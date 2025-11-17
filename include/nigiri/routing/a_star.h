a1d547f887758f8eaa213d291eb790eb1cab59a9
Philipp Lerche <philipp.lerche@gmx.de>
2025-11-17 18:33:35 +0545

    wip

--- include/nigiri/routing/a_star.h
+++ include/nigiri/routing/a_star.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "nigiri/routing/journey.h"
+#include "nigiri/routing/pareto_set.h"
 #include "nigiri/routing/tb/tb_data.h"
 #include "nigiri/types.h"
 
@@ -8,9 +9,26 @@ namespace nigiri {
 namespace routing {
 
 
-using arrivial_time_map = vector_map<tb::segment_idx_t, timezone_idx_t>;
-using day_index_map = vector_map<tb::segment_idx_t, day_idx_t>;
+// * The next three lines are the data structures needed as per info paper
+  // TODO: this should be a datetime already so the type needs to change or the second line is not needed
+using arrival_time_map = hash_map<tb::segment_idx_t, minutes_after_midnight_t>; // segment -> arrival_time mapping
+using day_index_map = hash_map<tb::segment_idx_t, day_idx_t>; // segment -> arrival_day
+using pred_table = hash_map<tb::segment_idx_t, tb::segment_idx_t>; // pred_table initialized with nullopt for everything
 
+struct queue_entry {
+  queue_entry(tb::segment_idx_t const s, std::uint8_t t) : segment_{s}, transfers_{t} {}
+
+  tb::segment_idx_t const segment_;
+  std::uint8_t transfers_;
+};
+
+pareto_set<journey> a_star(timetable const& tt,
+                           tb::tb_data const& tbd,
+                           unixtime_t const start_time,
+                           location_idx_t const source,
+                           location_idx_t const dest);
+
+// ! Not sure if this is needed
 struct at_map {
   at_map(tb::segment_idx_t const l, timezone_idx_t const at) : l_{l}, at_{at} {}
   friend bool operator>(at_map a,at_map b) { return a.at_ > b.at_; }
@@ -25,21 +43,5 @@ struct di_map {
   day_idx_t di_;
 };
 
-struct pred_table {
-
-};
-
-struct label {
-  label(tb::segment_idx_t const s, std::uint8_t t) : segment_{s}, transfers_{t} {}
-
-  tb::segment_idx_t segment_;
-  std::uint8_t transfers_;
-};
-
-
-journey a_star(tb::tb_data const&,
-               unixtime_t const start_time,
-               location_idx_t const source,
-               location_idx_t const dest);
 } // namespace routing
 } // namespace nigiri
